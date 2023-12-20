681792be8e082e2745b65fb131c45b187238e850
mathieu <mathieucazeres@gmail.com>
2023-12-20 21:36:04 +0100

    [feat] create table, insert methods

--- src/main/java/org/example/analyse/MainAnalyse.java
+++ src/main/java/org/example/analyse/MainAnalyse.java
@@ -3,18 +3,59 @@ package org.example.analyse;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.sql.*;
+
+/**
+ * TODO : Une méthode pour récuperer les id des produits qui
+ * sont supérieur soit a la médiane soit quartile
+ * poour une fois dans le parser save dans profil_expensive
+ * les produit ligen qui sont consiudéré
+ *
+ *
+ * TODO: parametrer le parse pour save les profil quand c'est le bon
+ */
 
 public class MainAnalyse {
+    /**
+     * meme db que dans le projet logger
+     */
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/logger";
+    private static final String DB_USER = "mathieu";
+    private static final String DB_PASSWORD = "12345678";
 
-    public static void main(String[] args) {
+    /**
+     * stat_prix_produit[0] => mediane
+     * stat_prix_produit[1] => quantile sup
+     */
+    private static double[] stat_prix_produit;
+    private static final SqlService SQL_SERVICE = new SqlService();
+
+    public static void main(String[] args) throws SQLException {
+
+        /**
+         * Creation des tables si besoin pour sauver les profils
+         */
+//        newTable();
+
+        /**
+         * Objetnir les statistique de la table product
+         */
+        stats();
 
 
-        String filePath = "/home/mathieu/Documents/EVOLUTION/logger-after/myLogs.xml"; // Spécifiez le chemin vers votre fichier
 
+        /**
+         * chemin vers le fichier de log du projet logger-after
+         * ce projet contient les logs inseré automatiquement par Spooon
+         */
+        String filePath = "/home/mathieu/Documents/EVOLUTION/logger-after/myLogs.xml";
+
+        // ouverture du fichier
         try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
             String line;
+            // lecture ligne par ligne
             while ((line = br.readLine()) != null) {
-                System.out.println(line );
+//                System.out.println(line );
 
 
                 parseLogLine(line); // Appel à la méthode pour analyser chaque ligne de log
@@ -24,6 +65,11 @@ public class MainAnalyse {
         }
     }
 
+
+    /**
+     * Methode qui permet de parser la ligne et de save les information dans les profils désiré
+     * @param logLine
+     */
     private static void parseLogLine(String logLine) {
         // Écrire ici la logique pour extraire les informations de chaque ligne de log
         // Utilisez des expressions régulières ou des opérations sur les chaînes pour obtenir les informations nécessaires
@@ -34,9 +80,106 @@ public class MainAnalyse {
         String method = parts[1].split(" : ")[1].trim(); // Récupère le nom de la méthode
 
         // Affiche les informations récupérées (à adapter à votre besoin)
-        System.out.println("Date/Time: " + dateTime);
-        System.out.println("Method: " + method);
+//        System.out.println("Date/Time: " + dateTime);
+//        System.out.println("Method: " + method);
         // Ajoutez le code pour extraire d'autres informations comme l'ID utilisateur, l'ID du produit, etc.
+    }
+
+
+    /**
+     * Uniquement pour creer tes tables
+     */
+    private static void newTable()
+    {
+        try {
+            Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+             SQL_SERVICE.createTables(connection);
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void stats()
+    {
+        try {
+            Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+            stat_prix_produit = SQL_SERVICE.calculateMedianAndUpperQuartile(connection);
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * Insertion dans le profil_read
+     */
+    private static void insertIntoProfilRead( long userId, long productId, String method, String jour_heure)
+    {
+        try {
+            Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+
+
+            SQL_SERVICE.insertIntoProfilRead(
+                    connection,
+                    userId,
+                    productId,
+                    method,
+                    jour_heure
+            );
+            // Fermeture des ressources
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Insertion dans le profil_write
+     */
+    private static void insertIntoProfilWrite(long userId, long productId, String method, String type, String jour_heure)
+    {
+        try {
+            Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+
+            SQL_SERVICE.insertIntoProfilWrite(
+                    connection,
+                    userId,
+                    productId,
+                    method,
+                    type,
+                    jour_heure
+            );
+            // Fermeture des ressources
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * Insertion dans le profil_expensive
+     */
+    private static void insertIntoProfilExpensive(long userId, long productId, String method, int prix, String jour_heure)
+    {
+        try {
+            Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+
+            SQL_SERVICE.insertIntoProfilExpensive(
+                    connection,
+                    userId,
+                    productId,
+                    method,
+                    prix,
+                    jour_heure
+            );
+            // Fermeture des ressources
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
 
---
+++ src/main/java/org/example/analyse/SqlService.java
@@ -0,0 +1,125 @@
+package org.example.analyse;
+
+
+import java.sql.*;
+
+public class SqlService {
+
+    public SqlService() {
+    }
+
+    public void createTables(Connection connection) throws SQLException {
+        Statement statement = connection.createStatement();
+
+        // Requête pour créer une table
+        String createReadTable = "CREATE TABLE profil_read ( " +
+                "id INT AUTO_INCREMENT PRIMARY KEY, " +
+                "id_user BIGINT, " +
+                "id_produit BIGINT, " +
+                "method VARCHAR(50), " +
+                "jour_heure VARCHAR(100),"+
+                "FOREIGN KEY (id_user) REFERENCES user(id)," +
+                "FOREIGN KEY (id_produit) REFERENCES product(id)" +
+                ");";
+
+        String createWriteTable = "CREATE TABLE profil_write ( " +
+                "id INT AUTO_INCREMENT PRIMARY KEY, " +
+                "id_user BIGINT, " +
+                "id_produit BIGINT, " +
+                "method VARCHAR(50), " +
+                "type VARCHAR(50), " +
+                "jour_heure VARCHAR(100),"+
+                "FOREIGN KEY (id_user) REFERENCES user(id)," +
+                "FOREIGN KEY (id_produit) REFERENCES product(id)" +
+                ");";
+
+
+        String createExpensiveTable = "CREATE TABLE profil_expensive ( " +
+                "id INT AUTO_INCREMENT PRIMARY KEY, " +
+                "id_user BIGINT, " +
+                "id_produit BIGINT, " +
+                "method VARCHAR(50), " +
+                "prix INT, " +
+                "jour_heure VARCHAR(100),"+
+                "FOREIGN KEY (id_user) REFERENCES user(id)," +
+                "FOREIGN KEY (id_produit) REFERENCES product(id)" +
+                ");";
+
+
+        // Exécution de la requête de création de table
+        statement.executeUpdate(createReadTable);
+        statement.executeUpdate(createWriteTable);
+        statement.executeUpdate(createExpensiveTable);
+        statement.close();
+        System.out.println("Table créée avec succès !");
+
+
+    }
+
+
+    public static void insertIntoProfilRead(Connection connection, long userId, long productId, String method, String jour_heure) throws SQLException {
+        String insertQuery = "INSERT INTO profil_read (id_user, id_produit, method, jour_heure) VALUES (?, ?, ?, ?)";
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(insertQuery)) {
+            preparedStatement.setLong(1, userId);
+            preparedStatement.setLong(2, productId);
+            preparedStatement.setString(3, method);
+            preparedStatement.setString(4, jour_heure);
+
+            preparedStatement.executeUpdate();
+        }
+    }
+
+    public static void insertIntoProfilWrite(Connection connection, long userId, long productId, String method, String type, String jour_heure) throws SQLException {
+        String insertQuery = "INSERT INTO profil_write (id_user, id_produit, method, type, jour_heure) VALUES (?, ?, ?, ?, ?)";
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(insertQuery)) {
+            preparedStatement.setLong(1, userId);
+            preparedStatement.setLong(2, productId);
+            preparedStatement.setString(3, method);
+            preparedStatement.setString(4, type);
+            preparedStatement.setString(5, jour_heure);
+
+            preparedStatement.executeUpdate();
+        }
+    }
+
+    public static void insertIntoProfilExpensive(Connection connection, long userId, long productId, String method, int prix, String jour_heure) throws SQLException {
+        String insertQuery = "INSERT INTO profil_expensive (id_user, id_produit, method, prix, jour_heure) VALUES (?, ?, ?, ?, ?)";
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(insertQuery)) {
+            preparedStatement.setLong(1, userId);
+            preparedStatement.setLong(2, productId);
+            preparedStatement.setString(3, method);
+            preparedStatement.setInt(4, prix);
+            preparedStatement.setString(5, jour_heure);
+
+            preparedStatement.executeUpdate();
+        }
+    }
+
+    public static double[] calculateMedianAndUpperQuartile(Connection connection) throws SQLException {
+        String query = "SELECT " +
+                "PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY price) OVER () AS mediane, " +
+                "PERCENTILE_CONT(0.75) WITHIN GROUP (ORDER BY price) OVER () AS quartile_superieur " +
+                "FROM product";
+
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query);
+             ResultSet resultSet = preparedStatement.executeQuery()) {
+
+            if (resultSet.next()) {
+                double mediane = resultSet.getDouble("mediane");
+                double quartileSuperieur = resultSet.getDouble("quartile_superieur");
+
+                System.out.println("Mediane : " + mediane);
+                System.out.println("Quartile supérieur : " + quartileSuperieur);
+                return new double[]{mediane, quartileSuperieur};
+            }
+
+
+        }
+        return null;
+    }
+
+}
