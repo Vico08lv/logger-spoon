cd2de820978acba251d38e7972fe92b5bc55b7e4
mathieu <mathieucazeres@gmail.com>
2023-12-21 22:36:31 +0100

    add comments

--- src/main/java/org/example/analyse/MainAnalyse.java
+++ src/main/java/org/example/analyse/MainAnalyse.java
@@ -8,10 +8,14 @@ import java.util.Arrays;
 import java.util.HashMap;
 
 /**
+ *
+ * @Note : Récapitulatif du travail qu'il reste a réaliser
+ *
  * TODO: continuer le parse pour save les profil quand c'est le bon
  * TODO : 1 -> Filter pour le cas profil_expensive
  * TODO : 2 -> Continuer pour les méthodes POST
  * TODO : 3 -> Réflexion sur la structure de l'api/des arguments/ des logs ( pour rapport) pour pb d'analyse
+ * !! TODO : refactor parseLogLine(..) car beaucoup trop long
  */
 
 public class MainAnalyse {
@@ -82,25 +86,39 @@ public class MainAnalyse {
         String dateTime = parts[0].split(" WARN ")[0].trim(); // Récupère la date et l'heure
         String method = parts[1].split(" : ")[1].trim(); // Récupère le nom de la méthode
 
+
+        /**
+         * Ici je save les params dans une hashmap
+         */
         HashMap<String,String> myHashMap = new HashMap<>();
         for (int i =2; i < parts.length; i++)
         {
             String[] key_val = parts[i].replace("[", "").replace("]", "").split(" : ");
 
+            /**
+             * Si pas un DTO -> ajout
+             */
             if (!key_val[0].equals("productDTO") & !key_val[0].equals("userDTO"))
             {
                 myHashMap.put(key_val[0],key_val[1]);
                 continue;
             }
 
+            /**
+             * Si DTO  ->  je cut pour recuperer les attributs
+             */
             String[] dtoList = key_val[1].split("\\(")[1].substring(0,key_val[1].split("\\(")[1].length()-1).split(", ");
-
-
-
+            // parcours la liste d'attributs
             for (String  e : dtoList)
             {
+                // Si mes clés ne sont pas deja contenue (gestion duc cas ou id deja trouvé par la roude)
+                // car Probleme pour methode POst (dans DTO id_produit = null car c'est la bd qui gère le numéro
+                // TODO : optimisation de ce point !! ?
                 if (!myHashMap.containsKey(e.split("=")[0]) && !e.split("=")[1].equals("null"))
                 {
+                    /**
+                     * j'ajoute chaque attribut et les valeurs associé
+                     */
                     myHashMap.put(e.split("=")[0],e.split("=")[1]);
                 }
             }
@@ -108,12 +126,15 @@ public class MainAnalyse {
 
         }
 
-        myHashMap.forEach((k,v) -> System.out.println(k +" | "+v));
+//        myHashMap.forEach((k,v) -> System.out.println(k +" | "+v));
 
-System.out.println(myHashMap.get("id") );
+//        System.out.println(myHashMap.get("id") );
         Long id_u = null;
         Long id_p = null;
 
+        /**
+         * Gestion du cas qui fais chier (ligne 112)
+         */
         if(myHashMap.get("id_user") != null)
         {
              id_u =Long.parseLong(myHashMap.get("id_user").replace(" ", ""));
@@ -144,11 +165,6 @@ System.out.println(myHashMap.get("id") );
                 break;
 
         }
-
-        // Affiche les informations récupérées (à adapter à votre besoin)
-//        System.out.println("Date/Time: " + dateTime);
-//        System.out.println("Method: " + method);
-        // Ajoutez le code pour extraire d'autres informations comme l'ID utilisateur, l'ID du produit, etc.
     }
 
     /**
